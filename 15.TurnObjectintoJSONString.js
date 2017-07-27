1fe9645c8be8b81bdbb224917999a1ccfe428b49
pavlinazgurova <pavlina.zgurova@abv.bg>
2017-07-27 18:57:13 +0300

    correction

--- 15.TurnObjectintoJSONString.js
+++ 15.TurnObjectintoJSONString.js
@@ -1,15 +1,15 @@
 function solve(input) {
-    let obj = {};
+    let personInfo = {};
     for(let i = 0; i < input.length; i++){
-        let str = input[i].split(' -> ');
-        obj[str[0]] = str[1];
-        if(str[0] == "grade"){
-            obj[str[0]] = Math.round(parseInt(str[1]));
+        let line = input[i].split(' -> ');
+        personInfo[line[0]] = line[1];
+        if(line[0] == "grade"){
+            personInfo[line[0]] = Math.round(parseInt(line[1]));
         }
-        if(str[0] == "age"){
-            obj[str[0]] = Math.round(parseInt(str[1]));
+        if(line[0] == "age"){
+            personInfo[line[0]] = Math.round(parseInt(line[1]));
         }
     }
-    console.log(JSON.stringify(obj));
+    console.log(JSON.stringify(personInfo));
 }
 solve([ 'name -> Angel', 'surname -> Georgiev', 'age -> 20', 'grade -> 6.00', 'date -> 23/05/1995', 'town -> Sofia']);
